967a16949a0c2b1fc1bab2e84d4ffb52406bfa89
Yu Chun Tsao <tsao84672776@gmail.com>
2020-09-02 21:01:06 +0800

    Initial commit

--- book/test.js
+++ book/test.js
@@ -1,6 +1,7 @@
 require(["gitbook"], function(gitbook) {
     gitbook.events.bind("page.change", function() {
         // do something
+        console.log("map map map")
     });
 
     gitbook.events.bind("exercise.submit", function() {
